05be88e5350c02347902f967ce237bbd9c1573ec
udaykumar <udaykumarsamidala57@gmail.com>
2025-10-06 08:20:16 +0530

    Updated logic in DBUtil.java

--- src/main/java/com/bean/DBUtil.java
+++ src/main/java/com/bean/DBUtil.java
@@ -6,10 +6,11 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    private static final String URL = "jdbc:mysql://shuttle.proxy.rlwy.net:26985/inventory";
+	private static final String URL = "jdbc:mysql://shuttle.proxy.rlwy.net:26985/inventory?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
     private static final String USER = "root";
     private static final String PASSWORD = "AUJrxXyvLxTXfsPDXMnFTTgpLcOaewkO";
+    
 
     static {
         try {
