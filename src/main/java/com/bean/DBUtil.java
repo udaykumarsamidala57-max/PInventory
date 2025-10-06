bacb209d779b76e9fa4920ed91e3f42fa2773705
udaykumarsamidala57-max <udaykumarsamidala57@gmail.com>
2025-10-06 06:30:01 +0530

    Update DBUtil.java

--- src/main/java/com/bean/DBUtil.java
+++ src/main/java/com/bean/DBUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    private static final String URL = "mysql://root:AUJrxXyvLxTXfsPDXMnFTTgpLcOaewkO@mysql.railway.internal:3306/inventory";
+    private static final String URL = "mysql:shuttle.proxy.rlwy.net:26985/inventory";
     private static final String USER = "root";
     private static final String PASSWORD = "AUJrxXyvLxTXfsPDXMnFTTgpLcOaewkO";
 
