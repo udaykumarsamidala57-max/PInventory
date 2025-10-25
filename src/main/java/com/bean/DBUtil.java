183115d7e4294e1a3b4bf69e95114ef4ed3ee198
udaykumar <udaykumarsamidala57@gmail.com>
2025-10-25 09:02:25 +0530

    Initial reupload with OTP feature

--- src/main/java/com/bean/DBUtil.java
+++ src/main/java/com/bean/DBUtil.java
@@ -6,9 +6,11 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-	private static final String URL = "jdbc:mysql://shuttle.proxy.rlwy.net:26985/inventory";
-    private static final String USER = "root";
-	private static final String PASSWORD = "123456789abcd19592025";
+	private static final String URL = 
+		    "jdbc:mysql://shuttle.proxy.rlwy.net:26985/inventory"
+		    + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&tcpKeepAlive=true";
+	private static final String USER = "root";
+	private static final String PASSWORD = "vSZVibKCzvcovcGjaLlxrTddrjiNPVQn";
 
 
 
